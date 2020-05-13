a5d70c2db37ab3653423ee4c9628803902c0a456
masijie <masijie@ilarge.cn>
2020-05-13 15:36:34 +0800

    更新build

---
+++ build/precache-manifest.8b2e844c4e08f787f9c1cac7f7299131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306936f0c59ee2e5fa3faed369a385c7",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "f1ec279448e64d461df1",
+    "url": "/react_test/static/css/2.da890b0b.chunk.css"
+  },
+  {
+    "revision": "6ebe5d695a4d3c144078",
+    "url": "/react_test/static/css/main.92e906d0.chunk.css"
+  },
+  {
+    "revision": "f1ec279448e64d461df1",
+    "url": "/react_test/static/js/2.acf9a114.chunk.js"
+  },
+  {
+    "revision": "6ebe5d695a4d3c144078",
+    "url": "/react_test/static/js/main.ed14b5d0.chunk.js"
+  },
+  {
+    "revision": "37c519b1b0bef28b0825",
+    "url": "/react_test/static/js/runtime~main.13abe56d.js"
+  }
+]);
